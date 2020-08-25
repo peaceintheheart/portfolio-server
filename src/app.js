23179e6abe5f1743e3b1c6d8a09ca1575ce1cad4
Chang <changc16@nih.gov>
2020-08-25 14:00:01 -0400

    init check-in

---
+++ src/app.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const { CLIENT_ORIGIN } = require('./config');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const nodemailer = require('nodemailer');
+const jsonParser = express.json();
+
+const app = express();
+app.use(cors());
+
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+
+var whitelist = ['https://holly-rogers.vercel.app', 'http://localhost:3000'];
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.post('/email', jsonParser, (req, res) => {
+    const { contact_name, email_from, email_body } = req.body;
+
+    async function main() {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            auth: {
+                user: 'hollymrogers12@gmail.com',
+                pass: process.env.email_pass,
+            },
+        });
+
+        let info = await transporter.sendMail({
+            from: email_from,
+            to: 'hollymrogers12@gmail.com',
+            subject: `Portfolio Contact Sheet - ${contact_name}`,
+            text: email_body,
+        });
+
+        res.send(info);
+    }
+
+    main().catch(console.error);
+});
+
+app.use(function errorHandler(error, req, res, next) {
+    let response;
+    if (NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } };
+    } else {
+        console.error(error);
+        response = { message: error.message, error };
+    }
+    res.status(500).json(response);
+});
+
+module.exports = app;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+
+    CLIENT_ORIGIN: process.env.DEV_ORIGIN || 'https://holly-rogers.vercel.app',
+}
